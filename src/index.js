e3897774373444151a751b21c8180fdcd58e3611
trabbster <david.avetisyan@trabbu.com>
2020-07-16 17:52:35 -0700

    save

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const word = 'Hello World';
+
+const words = ['Hello Earth', 'Goodbye Earth', 'Hello Mars'];
+
+words.forEach((word) => {console.log('Yo What Up'); There})
